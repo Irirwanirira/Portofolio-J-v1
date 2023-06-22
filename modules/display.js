c09ba337b1ce577cd7d3621badb8f8cfc281cdb5
Irirwanirira <irijoseph41@gmail.com>
2023-06-22 13:54:43 +0200

    Work on styles

--- modules/display.js
+++ modules/display.js
@@ -6,21 +6,33 @@ let display = '';
 function displayUi(project) {
   project.forEach((item) => {
     display += `
-    <a href="${item.liveLink}" class="card">
-            <img src=${item.image} alt=${item.title}/>
+    <div class="card">
+      <img src=${item.image} alt=${item.title}/>
 
-            <div>
-                    <h3>${item.title}</h3>
-                    <p>${item.description}
-                <ul>
-                <li> ${item.languaage[0]}</li>
-                <li> ${item.languaage[1]}</li>
-                <li> ${item.languaage[2]}</li>
-                </ul>
-            </div>
-    </a>
+      <div class="more-info">
+        <h3>${item.title}</h3>
+        <p>${item.description}
+
+          <ul class="technology">
+            <li> ${item.languaage[0]}</li>
+            <li> ${item.languaage[1]}</li>
+            <li> ${item.languaage[2]}</li>
+          </ul> 
+      
+        <div class="buttons-link">
+          <button class="live">
+            <a href="${item.liveLink}" >LIVE DEMO</a>
+          </button>
+          <button class="source">
+            <a href="${item.liveLink}" >SOURCE</a>
+          </button>
+        </div>
+
+      </div>
+    </div>
         `;
   });
   portofolio.innerHTML = display;
 }
 export default displayUi;
+
